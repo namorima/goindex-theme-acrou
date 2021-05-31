f9f9b31a732a7217d5e6ed8380132d2a5b702259
namorima <84259399+namorima@users.noreply.github.com>
2021-05-31 11:58:46 +0800

    update

--- src/views/common/Footer.vue
+++ src/views/common/Footer.vue
@@ -6,8 +6,8 @@
           <div class="tags has-addons">
             <a
               class="tag is-dark"
-              href="https://viperadnan-git.github.io"
-            >viperadnan</a>
+              href="https://github.com"
+            >NamoRima</a>
             <span class="tag is-light">
               <span class="icon">
                 <i class="fa fa-code"></i>
